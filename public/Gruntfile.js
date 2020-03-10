f671d0cfabf4bab5d114161baf3ccd5e9acbc849
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-10 04:29:13 +0000

    Gruntfile created, mongoose, bcryptjs, cssmin, csslint installed

---
+++ public/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: '/css',
+          src: ['*.css', '!*.min.css'],
+          dest: '/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    csslint: {
+
+      lax: {
+        options: {
+          import: false,
+          'order-alphabetical' :false
+        },
+        src: ['css/*.css','!*.min.css']
+      }
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin', 'csslint']);
+
+};
