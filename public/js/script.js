b8a573886d57c95501465f1db0e8b78079b7ead1
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-13 02:14:45 +0000

    View project frontend frame added (needs editing)

--- public/js/script.js
+++ public/js/script.js
@@ -38,35 +38,74 @@ $(document).ready(function(){
 	$('#registerProjectForm').submit(function(){
 		event.preventDefault();
 		console.log('submit');
- 		
+
  		projectName = $('#newProjectName').val();
 		projectBrief = $('#projectBrief').val();
 		projectImage = $('#projectPicture').val();
 		projectLink = $('#projectExternalLink').val();
 		userId = '23';// sessionStorage.getItem('userId');
- 		
+
  		if((projectName !== '') && (projectBrief !== '') && (projectImage !== '') && (projectLink !== '')){
  			console.log('testing');
  			$.ajax({
  				url : `${url}/registerProject`,
  				type : 'POST',
- 				data : { 
+ 				data : {
  					projectName : projectName ,
  					projectBrief : projectBrief ,
- 					projectImage : projectImage , 
- 					projectLink : projectLink , 
+ 					projectImage : projectImage ,
+ 					projectLink : projectLink ,
  					user_id : userId
  				},
  				success : function(data){
- 		
- 				}, 
+
+ 				},
  				error : function(){
  					alert('error: ');
  				}
  			});
  		} else{
- 			alert('Please fill in all fields'); 
+ 			alert('Please fill in all fields');
  		}
+	});
+
+
+	// View Project Cards
+	// Needs - BtnClick id | if parameter | author name info for card | Btn link for View More card btn 
+
+	$('#  ').click(function(){
+
+		if (  === projectUserId) {
+
+			$.ajax({
+				url : `${url}/viewProjects`,
+				type : 'GET',
+				dataType : 'json',
+				success : function(projectsFromMongo) {
+					console.log(projectsFromMongo);
+
+					document.getElementById('projectCards').innerHTML = "";
+					for (let i = 0; i < projectsFromMongo.length; i++) {
+						document.getElementById('projectCards').innerHTML +=
+						`<div class="card" style="width: 18rem;">
+							  <img src="${projectsFromMongo[i].projectImage}" class="card-img-top" alt="Project Image">
+
+								<div class="card-body">
+									<h5 class="card-title">${projectsFromMongo[i].projectName}</h5>
+									<p class="card-text">${   }</p>
+									<a href="#           " class="btn btn-primary">View More</a>
+							  </div>
+								<div id="cardFooter" class="card-footer text-muted">
+
+							  </div>
+						 </div>`;
+					}
+				}, //success
+				error:function(){
+					console.log('Error: Cannot call API');
+				}
+			});
+		}
 	});
 
 });
